f96437dd306a88362ad5cff5e91965816ca1c034
HyeonJeong Lim <applepie0526@naver.com>
2023-01-31 01:42:48 +0900

    first commit

--- src/Problem2812.java
+++ src/Problem2812.java
@@ -7,14 +7,14 @@ public class Problem2812 {
         StringTokenizer st = new StringTokenizer(in.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
         Stack<Integer> stack = new Stack<>();
 
         String s = in.readLine();
         int count = 0;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<N; i++){
             int n = s.charAt(i) - '0';
 
             while(!stack.isEmpty() && K > count && stack.peek() < n){
@@ -23,7 +23,7 @@ public class Problem2812 {
             }
             stack.push(n);
         }
-        for(int i=0; i<n-K; i++)
+        for(int i=0; i<N-K; i++)
             sb.append(stack.get(i));
         System.out.print(sb);
     }
