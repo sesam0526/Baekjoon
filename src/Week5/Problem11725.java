cb46b8f9df0d291f9b585f9f65d7f45db2af696f
sesam0526 <applepie0526@naver.com>
2023-02-25 15:16:32 +0900

    Solve: solve the problem by yourself

---
+++ src/Week5/Problem11725.java
@@ -0,0 +1,48 @@
+package Week5;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem11725 {
+    static ArrayList<Integer>[] arraylist;
+    static boolean[] visited;
+    static int[] parent;
+
+    public static void dfs(int V) {
+        visited[V] = true;
+
+        for (int e : arraylist[V]) {
+            if (!visited[e]) {
+                parent[e] = V;
+                dfs(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(in.readLine());
+        arraylist = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        parent = new int[N + 1];
+
+        for (int i = 0; i < arraylist.length; i++) {
+            arraylist[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            arraylist[from].add(to);
+            arraylist[to].add(from);
+        }
+
+        dfs(1);
+        for (int i = 2; i < parent.length; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+}
