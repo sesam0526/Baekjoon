c340489f95abe74cc6c79bd7e9360a48c687408f
sesam0526 <applepie0526@naver.com>
2023-03-05 16:42:24 +0900

    Style: reformat code

--- src/Week5/Problem4963.java
+++ src/Week5/Problem4963.java
@@ -8,11 +8,11 @@ public class Problem4963 {
     static boolean[][] visited;
     static int w, h;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        while(true){
+        while (true) {
             st = new StringTokenizer(in.readLine());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
@@ -22,17 +22,17 @@ public class Problem4963 {
             map = new int[h][w];
             visited = new boolean[h][w];
 
-            for(int i=0; i<h; i++){
+            for (int i = 0; i < h; i++) {
                 st = new StringTokenizer(in.readLine());
-                for(int j=0; j<w; j++){
+                for (int j = 0; j < w; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
             int island = 0;
-            for(int i=0; i<h; i++){
-                for(int j=0; j<w; j++){
-                    if(!visited[i][j] && map[i][j] == 1){
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
                         island++;
                         dfs(i, j);
                     }
